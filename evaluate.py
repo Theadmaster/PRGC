2e62106a598bc8bf5749a093b2f670ac3cdf6aa7
theadmaster <55393861+Theadmaster@users.noreply.github.com>
2024-06-05 16:30:23 +0800

    1

--- evaluate.py
+++ evaluate.py
@@ -25,6 +25,7 @@ parser.add_argument('--ex_index', type=str, default=1)
 parser.add_argument('--corpus_type', type=str, default="CA", help="NYT, WebNLG, NYT*, WebNLG*")
 parser.add_argument('--device_id', type=int, default=0, help="GPU index")
 parser.add_argument('--restore_file', default='last', help="name of the file containing weights to reload")
+parser.add_argument('--mode', default='test', help="mode")
 
 parser.add_argument('--corres_threshold', type=float, default=0.5, help="threshold of global correspondence")
 parser.add_argument('--rel_threshold', type=float, default=0.5, help="threshold of relation judgement")
