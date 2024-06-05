fc052e5e9498d9c18339431cc91223b614f6f2dd
theadmaster <55393861+Theadmaster@users.noreply.github.com>
2024-06-05 13:41:54 +0800

    22

--- script/train.sh
+++ script/train.sh
@@ -2,7 +2,7 @@
 pypath=$(dirname $(dirname $(readlink -f $0)))
 python $pypath/train.py \
 --ex_index=1 \
---epoch_num=100 \
+--epoch_num=10 \
 --device_id=0 \
 --corpus_type=CA \
 --ensure_corres \
